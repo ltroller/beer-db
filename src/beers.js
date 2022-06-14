0cc8ec516dbeb074ac209f4c04481057e605e737
Ludwig Troller <ltroller@apple.com>
2022-06-13 19:46:52 -0700

    chore(init): init

--- src/beers.js
+++ src/beers.js
@@ -1,14 +1,22 @@
+const path = 'https://ltroller.github.io/beer-db/';
+
 window.beers = [
   {
-    "name": "Jever Pils",
-    "description": "Lorem ipsum dolor",
-    "origin": "Germany"
+    name: 'Jever Pils',
+    description: 'Lorem ipsum dolor',
+    origin: 'Germany',
+    preview: `${path}jever.jpg`,
   },
   {
-    "name": "Berliner Pilsner",
-    "description": "Consetetur duis dolor",
-    "origin": "Germany"
-  }
+    name: 'Berliner Pilsner',
+    description: 'Consetetur duis dolor',
+    origin: 'Germany',
+    preview: `${path}berliner-pislner.jpg`,
+  },
+  {
+    name: 'Berliner Kindl',
+    description: 'The former West Berlin classic Pils',
+    origin: 'Germany',
+    preview: `${path}berliner-kindl.jpg`,
+  },
 ];
-
-
