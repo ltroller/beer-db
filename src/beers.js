651f9b3aee113f99fb10dde3090b9744dc788430
Ludwig T. Tröller <ltroller@digital-village.fr>
2022-06-13 19:33:46 -0700

    Create beers.js

---
+++ src/beers.js
@@ -0,0 +1,14 @@
+const beers = [
+  {
+    "name": "Jever Pils",
+    "description": "Lorem ipsum dolor",
+    "origin": "Germany"
+  },
+  {
+    "name": "Berliner Pilsner",
+    "description": "Consetetur duis dolor",
+    "origin": "Germany"
+  }
+];
+
+
